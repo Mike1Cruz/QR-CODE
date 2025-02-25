d5cb23cefb801a1b0960ed85a8c81a0aadd3484e
Mike1Cruz <mke6778@gmail.com>
2025-02-24 23:57:59 -0500

    Push 2

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-console.log("Server is running...");
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public")); // Serves files from the 'public' folder
+
+app.get("/", (req, res) => {
+  res.send("Hello, your server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
 
